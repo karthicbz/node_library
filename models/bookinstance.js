f4f030a2c8845ed7e409ac3590a631cf5f385a18
Karthick <pkarthicbz@live.in>
2023-06-09 11:52:14 +0530

    create bookinstance schema

---
+++ models/bookinstance.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
+const BookInstanceSchema = new schema({
+    book:{type:schema.Types.ObjectId, ref:'Book', required:true},
+    imprint:{type:String, required:true},
+    status:{
+        type:String,
+        required: true,
+        enum:["Available", "Maintenance", "Loaned", "Reserved"],
+        default: "Maintenance",
+    },
+    due_back: { type: Date, default: Date.now },
+});
+
+BookInstance.virtual('url').get(function(){
+    return(`/catalog/bookinstance/${this._id}`);
+});
+
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
