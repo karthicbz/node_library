94aeb6fd9952aab48ec13535b2c89fc27c5ae026
Karthick <pkarthicbz@live.in>
2023-06-14 16:54:45 +0530

    add node to gitignore

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
+
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
 
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-const mongoDB = 'mongodb+srv://admin:admin123@cluster0.frh5rdd.mongodb.net/local_library?retryWrites=true&w=majority';
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
+app.use(limiter);
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+app.use(compression());
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB =
+  "mongodb+srv://admin:admin123@cluster0.frh5rdd.mongodb.net/local_library?retryWrites=true&w=majority";
 
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
 
-app.use('/public', express.static('public'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use("/public", express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/catalog', catalogRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/catalog", catalogRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
